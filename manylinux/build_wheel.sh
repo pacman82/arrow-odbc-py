dccc709186faed431099f231aca062d0639e309a
Markus Klein <markus-klein@live.de>
2021-12-05 20:27:11 +0100

    fix manylinux build

--- manylinux/build_wheel.sh
+++ manylinux/build_wheel.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -xe
 
 # Compile wheels
-#for PYBIN in /opt/python/*/bin
-for PYBIN in /opt/python/cp37-cp37m/bin
-do
-    "${PYBIN}/pip" install --upgrade pip
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
+PYBIN="/opt/python/cp310-cp310/bin"
+
+"${PYBIN}/pip" install --upgrade pip
+"${PYBIN}/pip" wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in /wheelhouse/arrow_odbc-*.whl
