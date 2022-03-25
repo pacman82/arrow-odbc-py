71954c20781d554ae51212c2dcdab1cb33669dc5
Markus Klein <markus-klein@live.de>
2022-03-25 18:32:26 +0100

    bump version to 0.1.18

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Markus Klein"
 author = "Markus Klein"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.17"
+release = "0.1.18"
 
 
 # -- General configuration ---------------------------------------------------
