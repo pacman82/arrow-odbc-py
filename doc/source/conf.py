de56b76a51ccdb2d05987654ece576f8149cdee1
Markus Klein <markus-klein@live.de>
2022-05-06 08:35:14 +0200

    fix version 0.1.20

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Markus Klein"
 author = "Markus Klein"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.120"
+release = "0.1.20"
 
 
 # -- General configuration ---------------------------------------------------
