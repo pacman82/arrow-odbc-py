ccf86e466eba31d4d021a3b6622c10370a16dd0c
Markus Klein <markus-klein@live.de>
2023-04-09 14:06:46 +0200

    increase version to 1.0.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Markus Klein"
 author = "Markus Klein"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.14"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
