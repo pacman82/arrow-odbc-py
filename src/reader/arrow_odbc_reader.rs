d3262e101394db55986255dd31330cdff4988d1d
Markus Klein <markus-klein@live.de>
2023-10-08 18:13:44 +0200

    ArrowOdbcReader is now a custom enum.
    This allows the easier addition of more states in the future

--- src/reader/arrow_odbc_reader.rs
+++ src/reader/arrow_odbc_reader.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+
 use crate::error::ArrowOdbcError;
 use arrow::{
     array::{Array, StructArray},
@@ -12,7 +14,12 @@ use arrow_odbc::{
 
 /// Opaque type holding all the state associated with an ODBC reader implementation in Rust. This
 /// type also has ownership of the ODBC Connection handle.
-pub struct ArrowOdbcReader(Option<OdbcReader<CursorImpl<StatementConnection<'static>>>>);
+pub enum ArrowOdbcReader {
+    /// The last result set has been extracted from the cursor. There is nothing more to fetch and
+    /// all associated resources have been deallocated
+    NoMoreResultSets,
+    Reader(OdbcReader<CursorImpl<StatementConnection<'static>>>),
+}
 
 impl ArrowOdbcReader {
     pub fn new(
@@ -22,17 +29,20 @@ impl ArrowOdbcReader {
     ) -> Result<Self, arrow_odbc::Error> {
         let schema = None; // Autodiscover schema information
         let reader = OdbcReader::with(cursor, batch_size, schema, buffer_allocation_options)?;
-        Ok(Self(Some(reader)))
+        Ok(Self::Reader(reader))
     }
 
     pub fn empty() -> Self {
-        Self(None)
+        Self::NoMoreResultSets
     }
 
     pub fn next_batch(
         &mut self,
     ) -> Result<Option<(FFI_ArrowArray, FFI_ArrowSchema)>, ArrowOdbcError> {
-        let next = self.0.as_mut().and_then(OdbcReader::next).transpose()?;
+        let next = match self {
+            ArrowOdbcReader::NoMoreResultSets => None,
+            ArrowOdbcReader::Reader(reader) => reader.next().transpose()?,
+        };
         let next = if let Some(batch) = next {
             let struct_array: StructArray = batch.into();
             let array_data = struct_array.to_data();
@@ -51,34 +61,38 @@ impl ArrowOdbcReader {
         batch_size: usize,
         buffer_allocation_options: BufferAllocationOptions,
     ) -> Result<bool, ArrowOdbcError> {
-        // None in case this reader has already moved past the last result set.
-        if self.0.is_none() {
-            return Ok(false);
-        }
-        let inner = self.0.take().unwrap();
+        // Move self into a temporary instance we own, in order to take ownership of the inner
+        // reader and move it to a different typestate.
+        let mut tmp_self = ArrowOdbcReader::NoMoreResultSets;
+        swap(self, &mut tmp_self);
+        let inner = match tmp_self {
+            ArrowOdbcReader::NoMoreResultSets => return Ok(false),
+            ArrowOdbcReader::Reader(inner) => inner,
+        };
         let cursor = inner.into_cursor()?;
         if let Some(cursor) = cursor.more_results()? {
             // There is another result set. Let us create a new reader
-            self.0 = Some(OdbcReader::with(
-                cursor,
-                batch_size,
-                None,
-                buffer_allocation_options,
-            )?);
-        };
-        Ok(self.0.is_some())
+            let reader = OdbcReader::with(cursor, batch_size, None, buffer_allocation_options)?;
+            *self = ArrowOdbcReader::Reader(reader);
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     pub fn schema(&self) -> Result<FFI_ArrowSchema, ArrowOdbcError> {
-        let schema_ffi = if let Some(inner) = self.0.as_ref() {
-            let schema_ref = inner.schema();
-            let schema = &*schema_ref;
-            schema.try_into()?
-        } else {
-            // A schema with no columns. Different from FFI_ArrowSchema empty, which can not be
-            // imported into pyarrow
-            let schema = Schema::empty();
-            schema.try_into()?
+        let schema_ffi = match self {
+            ArrowOdbcReader::NoMoreResultSets => {
+                // A schema with no columns. Different from FFI_ArrowSchema empty, which can not be
+                // imported into pyarrow
+                let schema = Schema::empty();
+                schema.try_into()?
+            }
+            ArrowOdbcReader::Reader(inner) => {
+                let schema_ref = inner.schema();
+                let schema = &*schema_ref;
+                schema.try_into()?
+            }
         };
         Ok(schema_ffi)
     }
