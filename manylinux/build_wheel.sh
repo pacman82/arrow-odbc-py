90daf3c5b0294be0c2aba418665eae2633b2d27b
Markus Klein <markus-klein@live.de>
2025-01-31 12:55:17 +0100

    fix: Use auditwheel repair to create manylinux wheel. This fixes an issue with the SHA256 hash of the WHEEL file in the dist info

--- manylinux/build_wheel.sh
+++ manylinux/build_wheel.sh
@@ -6,37 +6,19 @@ PYBIN="/opt/python/cp310-cp310/bin"
 "${PYBIN}/pip" install --upgrade pip
 "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 
-# Do not bundle unixODBC with the manylinux wheel
-#
-# * Won't work well, because ODBC lib has to dynamically load drivers from the
-#  system anyway.
-# * Unclear (to me) if we could redistribute unixODBC that way, since it is
-#   GPL.
-#
-# So we won't call auditwheel repair
 
 mkdir /io/dist
 
 for f in /wheelhouse/arrow_odbc-*.whl
 do
-    # We won't do that, see above
-    # auditwheel repair "${whl}" -w /io/dist/
-
-    # Instead we unzip the wheel edit the dist info and rename it ourselfs, so we can upload it to PyPi
-
-    # f looks like e.g. /wheelhouse/arrow_odbc-0.1.8-py3-none-linux_x86_64.whl
-    # trunk e.g. /wheelhouse/arrow_odbc-0.1.8
-    trunk=${f%-py3-none-linux_x86_64.whl}
-    # E.g. 0.1.8
-    ver=${trunk#*-}
-
-    mv $f /wheelhouse/arrow_odbc-${ver}.zip
-    unzip /wheelhouse/arrow_odbc-${ver}.zip -d /wheelhouse/arrow_odbc-${ver}-edit
-
-    cp -f /io/manylinux/WHEEL "/wheelhouse/arrow_odbc-${ver}-edit/arrow_odbc-${ver}.dist-info/WHEEL"
-
-    cd /wheelhouse/arrow_odbc-${ver}-edit/
-    zip -rv /io/dist/arrow_odbc-${ver}-py3-none-manylinux_2_17_x86_64.manylinux2014_x86_64.whl .
-    cd -
+    # Do not bundle unixODBC with the manylinux wheel
+    #
+    # * Won't work well, because ODBC lib has to dynamically load drivers from the
+    #  system anyway.
+    # * Unclear (to me) if we could redistribute unixODBC that way, since it is
+    #   GPL.
+    #
+    # Other than that auditwheel will change the tag fro linux_x86_64 to manylinux_2_17_x86_64 and
+    # manylinux2016_x86_64
+    auditwheel repair --exclude libodbc.so.* "${f}" -w /io/dist/
 done
-
