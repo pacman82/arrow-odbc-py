9dc3d43de8839c7f4f89fb221086070371489ddc
Markus Klein <markus-klein@live.de>
2025-09-21 13:38:30 +0200

    ci: Install unixODBC via `yum` for manylinux build.
    This fixes the linker error, as it is now installed in a path searched
    by the build system.

--- manylinux/build_wheel.sh
+++ manylinux/build_wheel.sh
@@ -17,8 +17,5 @@ do
     #  system anyway.
     # * Unclear (to me) if we could redistribute unixODBC that way, since it is
     #   GPL.
-    #
-    # Other than that auditwheel will change the tag fro linux_x86_64 to manylinux_2_17_x86_64 and
-    # manylinux2016_x86_64
     auditwheel repair --exclude libodbc.so.* "${f}" -w /io/dist/
 done
