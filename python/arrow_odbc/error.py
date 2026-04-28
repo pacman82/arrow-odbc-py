6a1aa176c20fd31b63d317eb099bc7034bcdce30
Markus Klein <markus-klein@live.de>
2026-04-28 09:36:58 +0000

    fix: `Error` now sets the message eagerly on the Exception super class

--- python/arrow_odbc/error.py
+++ python/arrow_odbc/error.py
@@ -1,3 +1,5 @@
+from cffi import FFI
+
 from .arrow_odbc import ffi, lib
 
 
@@ -6,23 +8,23 @@ class Error(Exception):
     An error emmited by the arrow-odbc-py bindings.
     """
 
-    def __init__(self, handle):
-        self.handle = handle
-
-    def __del__(self):
-        lib.arrow_odbc_error_free(self.handle)
+    def __init__(self, handle: FFI.CData):
+        raw = ffi.string(lib.arrow_odbc_error_message(handle))
+        # ffi.string returns bytes or str. For us it will always return bytes.3
+        assert isinstance(raw, bytes)
+        message = raw.decode("utf-8")
+        lib.arrow_odbc_error_free(handle)
+        super().__init__(message)
 
     def message(self) -> str:
         """
         A string describing the error.
         """
-        return ffi.string(lib.arrow_odbc_error_message(self.handle)).decode("utf-8")
-
-    def __str__(self) -> str:
-        return self.message()
+        assert isinstance(self.args[0], str)
+        return self.args[0]
 
 
-def raise_on_error(error_out):
+def raise_on_error(error_out: FFI.CData):
     """
     Raises if the argument points to an error
     """
